0cf81e32a2f6c3a1d478a0a526557a8b50b911d8
Leonardo Passos <lpassos@gsd.uwaterloo.ca>
2011-05-08 12:38:20 -0400

    New adapters

---
+++ src/gsd/cdl/parser/IMLParser.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2010 Steven She <shshe@gsd.uwaterloo.ca>
+ * and Thorsten Berger <berger@informatik.uni-leipzig.de>
+ *
+ * This file is part of CDLTools.
+ *
+ * CDLTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CDLTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CDLTools.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package gsd.cdl
+
+import model._
+import gsd.iml.parser.ImlParser 
+
+object EcosIML {
+
+  def parseFile(file : String) : List[Node] =  {
+    /* TODO */
+    return null
+  }
+}
---
+++ src/gsd/cdl/parser/adapter/AstAdapters.scala
@@ -0,0 +1,243 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gsd.cdl.model
+
+import gsd.iml.ast.expression._
+import gsd.cdl.formula._
+import gsd.iml.ast.feature._
+import gsd.iml.ast.constraint._
+
+object ImlExpressionToCDLExpression {
+
+  def apply(e:Expression) : CDLExpression = {
+
+    if (e.isInstanceOf[AndExpression]) {
+      val exp = e.asInstanceOf[AndExpression]
+      return new And(ImlExpressionToCDLExpression(exp.getLeft()),
+                      ImlExpressionToCDLExpression(exp.getRight()))
+    }
+
+    if (e.isInstanceOf[OrExpression]) {
+      val exp = e.asInstanceOf[OrExpression]
+      return new Or(ImlExpressionToCDLExpression(exp.getLeft()),
+                     ImlExpressionToCDLExpression(exp.getRight()))
+    }
+
+    if (e.isInstanceOf[BitwiseAndExpression]) {
+      val exp = e.asInstanceOf[BitwiseAndExpression]
+      return new BtAnd(ImlExpressionToCDLExpression(exp.getLeft()),
+                        ImlExpressionToCDLExpression(exp.getRight()))
+    }
+
+    if (e.isInstanceOf[BitwiseOrExpression]) {
+      val exp = e.asInstanceOf[BitwiseOrExpression]
+      return new BtOr(ImlExpressionToCDLExpression(exp.getLeft()),
+                      ImlExpressionToCDLExpression(exp.getRight()))
+    }
+
+    if (e.isInstanceOf[BitwiseXorExpression]) {
+      val exp = e.asInstanceOf[BitwiseXorExpression]
+      return new BtXor(ImlExpressionToCDLExpression(exp.getLeft()),
+                        ImlExpressionToCDLExpression(exp.getRight()))
+    }
+
+    if (e.isInstanceOf[BitwiseLeftShiftExpression]) {
+      val exp = e.asInstanceOf[BitwiseLeftShiftExpression]
+      return new BtLeft(ImlExpressionToCDLExpression(exp.getLeft()),
+                        ImlExpressionToCDLExpression(exp.getRight()))
+    }
+
+    if (e.isInstanceOf[BitwiseRightShiftExpression]) {
+      val exp = e.asInstanceOf[BitwiseRightShiftExpression]
+      return new BtRight(ImlExpressionToCDLExpression(exp.getLeft()),
+                          ImlExpressionToCDLExpression(exp.getRight()))
+    }
+
+    if (e.isInstanceOf[FunctionCallExpression]) {
+      val exp = e.asInstanceOf[FunctionCallExpression]
+      val args = List[CDLExpression]()
+
+      val iterator = exp.getArguments().iterator ;
+      while(iterator.hasNext) {
+        args.+:(iterator.next)
+      }
+
+      return new FunctionCall(exp.getName(), args)
+   }
+
+    if (e.isInstanceOf[EqualExpression]) {
+      val exp = e.asInstanceOf[EqualExpression]
+      return new Eq(ImlExpressionToCDLExpression(exp.getLeft()),
+                    ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[NotEqualExpression]) {
+      val exp = e.asInstanceOf[NotEqualExpression]
+      return new NEq(ImlExpressionToCDLExpression(exp.getLeft()),
+                     ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[LessThanExpression]) {
+      val exp = e.asInstanceOf[LessThanExpression]
+      return new LessThan(ImlExpressionToCDLExpression(exp.getLeft()),
+                          ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[LessThanEqualExpression]) {
+      val exp = e.asInstanceOf[LessThanEqualExpression]
+      return new LessThanOrEq(ImlExpressionToCDLExpression(exp.getLeft()),
+                              ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[GreaterThanExpression]) {
+      val exp = e.asInstanceOf[GreaterThanExpression]
+      return new GreaterThan(ImlExpressionToCDLExpression(exp.getLeft()),
+                             ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[GreaterThanEqualExpression]) {
+      val exp = e.asInstanceOf[GreaterThanEqualExpression]
+      return new GreaterThanOrEq(ImlExpressionToCDLExpression(exp.getLeft()),
+                                 ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[ModExpression]) {
+      val exp = e.asInstanceOf[ModExpression]
+      return new Mod(ImlExpressionToCDLExpression(exp.getLeft()),
+                      ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[PlusExpression]) {
+      val exp = e.asInstanceOf[PlusExpression]
+      return new Plus(ImlExpressionToCDLExpression(exp.getLeft()),
+                      ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[MinusExpression]) {
+      val exp = e.asInstanceOf[MinusExpression]
+      return new Minus(ImlExpressionToCDLExpression(exp.getLeft()),
+                        ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[TimesExpression]) {
+      val exp = e.asInstanceOf[TimesExpression]
+      return new Times(ImlExpressionToCDLExpression(exp.getLeft()),
+                       ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[DivideExpression]) {
+      val exp = e.asInstanceOf[DivideExpression]
+      return new Div(ImlExpressionToCDLExpression(exp.getLeft()),
+                      ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[DotExpression]) {
+      val exp = e.asInstanceOf[DotExpression]
+      return new Dot(ImlExpressionToCDLExpression(exp.getLeft()),
+                     ImlExpressionToCDLExpression(exp.getRight))
+    }
+
+    if (e.isInstanceOf[NotExpression]) {
+      val exp = e.asInstanceOf[NotExpression]
+      return new Not(ImlExpressionToCDLExpression(exp.getExpression()))
+    }
+
+    if (e.isInstanceOf[IdentifierExpression]) {
+      val exp = e.asInstanceOf[IdentifierExpression]
+      return new Identifier(exp.get())
+    }
+
+    if (e.isInstanceOf[LongLiteralExpression]) {
+      val exp = e.asInstanceOf[LongLiteralExpression]
+      return new LongIntLiteral(exp.get().longValue())
+    }
+
+    if (e.isInstanceOf[StringLiteralExpression]) {
+      val exp = e.asInstanceOf[StringLiteralExpression]
+      return new StringLiteral(exp.get())
+    }
+
+    throw new Exception("Unknown adapter for " + e.getClass().getName())
+  }
+}
+
+object ImlFeatureToNode {
+  def apply(f:Feature) : Node = {
+    /* TODO */
+
+
+    return null
+  }
+}
+
+object ImlFeatureTypeToCDLType {
+  def apply(t:FeatureType) : CDLType = {
+    if (t.isInstanceOf[ComponentFeatureType])
+      return ComponentType
+
+    if (t.isInstanceOf[InterfaceFeatureType])
+      return InterfaceType
+
+    if (t.isInstanceOf[PackageFeatureType])
+      return PackageType
+
+    return OptionType
+  }
+}
+
+object ImlFlavorToCDLFlavor {
+  def apply(f:gsd.iml.ast.flavor.Flavor) : gsd.cdl.model.Flavor = {
+    if (f.isInstanceOf[gsd.iml.ast.flavor.NoneFlavor])
+      return gsd.cdl.model.NoneFlavor
+
+    if (f.isInstanceOf[gsd.iml.ast.flavor.BoolFlavor])
+      return gsd.cdl.model.BoolFlavor
+
+    if (f.isInstanceOf[gsd.iml.ast.flavor.DataFlavor])
+      return gsd.cdl.model.DataFlavor ;
+
+    return gsd.cdl.model.BoolDataFlavor ;
+  }
+}
+
+object ImlLegalValuesConstraintToCDLValuesOption {
+  def apply(legalValues : LegalValuesConstraint) : LegalValuesOption =  {
+    val ranges = List[Range]()
+    val iterator = legalValues.getExpressions().iterator
+
+    while(iterator.hasNext) {
+      val exp = iterator.next
+      if (exp.isInstanceOf[IntervalExpression])
+        ranges.+:(ImlIntervalExpressionToMinMaxRange(exp.asInstanceOf[IntervalExpression]))
+      else 
+        ranges.+:(new SingleValueRange(ImlExpressionToCDLExpression(exp)))
+    }    
+    return new LegalValuesOption(ranges)
+  }
+}
+
+object ImlIntervalExpressionToMinMaxRange {
+  def apply(e: IntervalExpression) : Range = {
+      val interval = e.asInstanceOf[IntervalExpression] ;
+      return new MinMaxRange(ImlExpressionToCDLExpression(interval.getFrom()),
+                             ImlExpressionToCDLExpression(interval.getTo()))
+  }
+}
+
+object ListOfImlExpressionToListOfCDLExpression  {
+  def apply(listExps: java.util.List[Expression]) : List[CDLExpression] = {
+    val res = List[CDLExpression]()
+    if (! gsd.iml.util.CollectionsUtils.isEmpty(listExps)) {
+      val iterator = listExps.iterator
+      while(iterator.hasNext) {
+        res.+: (ImlExpressionToCDLExpression(iterator.next))
+      }
+    }
+    return res
+  }
+}
+
+
