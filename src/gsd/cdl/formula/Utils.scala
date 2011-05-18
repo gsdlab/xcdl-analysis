c9d996e15120d2a11cb34d5df6591ce0207ce0dc
mnovakov-gsd <mnovakov@gsd.uwaterloo.ca>
2011-05-18 19:47:34 -0400

    This message is not empty

--- src/gsd/cdl/formula/Utils.scala
+++ src/gsd/cdl/formula/Utils.scala
@@ -1,11 +1,21 @@
 package gsd.cdl.formula
 
 object Utils {
+  def removeTrailingQuotesFrom(string:String):String = {
+    if (string.charAt(0) == '"' && string.charAt(string.length - 1) == '"') {
+      string.substring(1, string.length - 1)
+    } else {
+      string
+    }
+  }
   def guardEnumValue(value:String): String = {
 //		if (value.startsWith("\"") && value.endsWith("\"")) {
 //			value.substring(1, value.length - 1)
 //		} else
 //			value
     value.replaceAll("\"", "")
+  }
+  def guardEnumValue(value:Int): String = {
+    guardEnumValue("" + value)
   }
 }
